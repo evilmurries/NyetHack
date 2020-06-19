b63b4de27d61bd978ea00eb1225b391c7632ae56
xpy <cpiwarski01@gmail.com>
2020-06-19 07:07:54 -0700

    everything before tavern

--- src/Game.kt
+++ src/Game.kt
@@ -1,21 +1,6 @@
 
 const val MAX_EXPERIENCE: Int = 5000
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds, but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-    return healthStatus
-}
-
 fun main(args: Array<String>) {
     val name: String = "Xpy"
     var healthPoints: Int = 89
@@ -23,10 +8,37 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = getAuraColor(auraVisible)
 
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    castFireball()
+}
+
+private fun getAuraColor(auraVisible: Boolean): String = if (auraVisible) "GREEN" else "NONE"
+
+private fun castFireball(numFireballs: Int = 2) = println(
+    "A glass of Fireball springs into existence (x$numFireballs)")
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed) {
+        "has some minor wounds, but is healing quite quickly!"
+    } else {
+        "has some minor wounds."
+    }
+    in 15..74 -> "looks pretty hurt."
+    else -> "is in awful condition!"
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
     println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$name $healthStatus")
 }
