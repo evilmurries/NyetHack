aa0ac4db9df619d06e29450efaa55e3ce02e2524
Christopher Piwarski <christopher.piwarski@betterhelp.com>
2020-07-07 16:43:40 -0700

    meta: Main game logic

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -3,18 +3,44 @@ package com.bignerdranch.nyethack
 const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
-
-    val player = Player("xpy")
-
-
-    val auraColor = player.getAuraColor()
-
-    printPlayerStatus(player)
-    player.castFireball()
+    Game.play()
 }
 
-private fun printPlayerStatus(player: Player) {
-    println("Aura: ${player.getAuraColor()} " +
-    "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()}")
+object Game {
+
+    val player = Player("xpy")
+    var currentRoom = TownSquare()
+
+    init {
+        println("Welcome, adventurer.")
+        player.castFireball()
+    }
+
+    fun play() {
+        while (true) {
+
+            // Room info
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            // Player Status
+            printPlayerStatus(player)
+
+            // User feedback loop
+            print("> Enter your command: ")
+            println("Last command: ${readLine()}")
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println("Aura: ${player.getAuraColor()} " +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+    }
 }
