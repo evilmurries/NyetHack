84af9af9049b707c481c2aeec0d92860a4dfad3d
xpy <cpiwarski01@gmail.com>
2020-07-21 07:25:17 -0700

    chapter 17: Generics - completed

--- src/com/bignerdranch/nyethack/sandbox.kt
+++ src/com/bignerdranch/nyethack/sandbox.kt
@@ -1,19 +1,41 @@
 package com.bignerdranch.nyethack
 
-class LootBox<T>(item: T) {
+class LootBox<T : Loot>(vararg item: T) {
     var open = false
-    private var loot: T = item
+    private var loot: Array<out T> = item
 
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    operator fun get(index: Int): T? = loot[index].takeIf { open }
+
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
+    }
+
+    fun <R> fetch(item: Int, lootModFunction: (T) -> (R)): R? {
+        return lootModFunction(loot[item]).takeIf { open }
     }
 }
 
-class Fedora(val name: String, val value: Int)
+open class Loot(val value: Int)
 
-class Coin(val value: Int)
+class Fedora(val name: String, value: Int) : Loot(value)
+
+class Coin(value: Int) : Loot(value)
 
 fun main(args: Array<String>) {
-    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic looking fedora", 15))
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a dazzling magenta fedora", 25), Fedora("A generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch(1)?.run {
+        println("You retrieve a $name from the box!")
+    }
+
+    val fedora = lootBoxOne[1]
+    fedora?.let { println(it.name) }
+
+    val coin = lootBoxOne.fetch(0) {
+        Coin(it.value * 3)
+    }
+    coin?.let { println(it.value) }
 }
---
+++ src/com/bignerdranch/nyethack/variance.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.nyethack
+
+class Barrel<in T>(item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("A generic looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+
+    fedoraBarrel = lootBarrel
+}
